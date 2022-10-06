eaa0a4a7c1e55cd46b4d5bc52c76361b52750a4c
Hasith Methmal Jayasekara <hasith.m@eyepax.com>
2022-10-06 17:55:09 +0530

    [feat] added material design scaffold to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
         ),
+        body: const Text('This is my default text!'),
       ),
     );
+  }
+}
