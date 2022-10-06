a65e9876b59f06f46cb112407fac7f685d8495a0
Hasith Methmal Jayasekara <hasith.m@eyepax.com>
2022-10-06 18:54:24 +0530

    [feat] sample buttons and function calls

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,37 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void answerQuestion() {
+    print('Answer Chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+                onPressed: answerQuestion, child: const Text('Answer 1')),
+            ElevatedButton(
+                onPressed: () => print('Answer 2 chosen!'),
+                child: const Text('Answer 2')),
+            ElevatedButton(
+                onPressed: () {
+                  print('Damn boy');
+                },
+                child: const Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
